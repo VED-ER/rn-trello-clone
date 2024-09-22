3a4dfd93ff8d2111fe03e0e1ead964b6598973f6
Vedran Erak <vedran.erak@snapguest.si>
2024-09-22 15:37:08 +0200

    added boards screen, added individual board screen with custom header with blured view (replaced Slot in layout nav to make individual board screen go over tabs in stack nav)

--- app/(authenticated)/(tabs)/boards/index.tsx
+++ app/(authenticated)/(tabs)/boards/index.tsx
@@ -1,9 +1,60 @@
-import { Text, View } from "react-native";
+import { Alert, FlatList, RefreshControl, StyleSheet, Text, View } from "react-native";
+import { useCallback, useState } from "react";
+import { Board } from "@/types/enums";
+import { Link, useFocusEffect } from "expo-router";
+import { useSupabase } from "@/context/SupabaseContext";
 
 export default function Boards() {
+    const [boards, setBoards] = useState<Board[]>([]);
+    const [refreshing, setRefreshing] = useState(false);
+    const { getBoards } = useSupabase();
+    console.log(boards);
+    useFocusEffect(
+        useCallback(() => {
+            loadBoards();
+        }, []),
+    );
+
+    const loadBoards = async () => {
+        try {
+            const data = await getBoards!();
+            setBoards(data);
+        } catch (e: any) {
+            Alert.alert("Error creating board", e.message);
+        }
+    };
+
+    const renderItem = ({ item }: { item: Board }) => (
+        <Link
+            // @ts-ignore
+            href={`/(authenticated)/board/${item.id}?bg=${encodeURIComponent(item.background)}`}
+            key={item.id}
+        >
+            <View className={"py-4 px-3 gap-x-4 flex-row items-center"}>
+                <View style={{ backgroundColor: item.background }} className={"h-8 w-8 rounded"} />
+                <Text className={"text-sm"}>{item.title}</Text>
+            </View>
+        </Link>
+    );
+
+    const keyExtractor = (item: Board): string => item.id;
+
+    const ItemSeparatorComponent = () => <View className={"h-[0.5px] bg-grey"} />;
+
     return (
-        <View>
-            <Text>Boards</Text>
-        </View>
+        <FlatList
+            data={boards}
+            renderItem={renderItem}
+            keyExtractor={keyExtractor}
+            ItemSeparatorComponent={ItemSeparatorComponent}
+            contentContainerStyle={{
+                backgroundColor: "white",
+                marginTop: 20,
+                borderColor: "grey",
+                borderTopWidth: StyleSheet.hairlineWidth,
+                borderBottomWidth: StyleSheet.hairlineWidth,
+            }}
+            refreshControl={<RefreshControl refreshing={refreshing} onRefresh={loadBoards} />}
+        />
     );
 }
---
+++ app/(authenticated)/board/[id].tsx
@@ -0,0 +1,95 @@
+import { Alert, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { Link, router, Stack, useLocalSearchParams } from "expo-router";
+import { BlurView } from "expo-blur";
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import { colors } from "@/constants/tailwind-colors";
+import { useEffect, useState } from "react";
+import { useHeaderHeight } from "@react-navigation/elements";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useSupabase } from "@/context/SupabaseContext";
+import { Board } from "@/types/enums";
+
+export default function Id() {
+    const { id, bg } = useLocalSearchParams<{ id: string; bg?: string }>();
+    const { getBoardInfo } = useSupabase();
+    const [board, setBoard] = useState<Board>();
+    const { top } = useSafeAreaInsets();
+    const headerHeight = useHeaderHeight();
+
+    useEffect(() => {
+        if (id) {
+            loadBoardInfo();
+        }
+    }, [id]);
+
+    const loadBoardInfo = async () => {
+        if (!id) return;
+
+        try {
+            const data = await getBoardInfo!(id);
+            setBoard(data);
+        } catch (e: any) {
+            Alert.alert("Error loading board info", e.message);
+        }
+    };
+
+    const CustomHeader = () => (
+        <BlurView intensity={80} tint="dark" style={{ paddingTop: top }}>
+            <View className={"flex-row items-center gap-3 bg-none px-4 h-16"}>
+                <TouchableOpacity
+                    onPress={() => {
+                        router.dismiss();
+                    }}
+                >
+                    <Ionicons name="close" size={24} color={colors.fontLight} />
+                </TouchableOpacity>
+
+                <View style={{ flex: 1 }}>
+                    <Text style={{ color: colors.fontLight, fontSize: 16 }}>{board?.title}</Text>
+                    <Text style={{ color: colors.fontLight, fontSize: 12 }}>
+                        Workspace of {(board as any)?.users.first_name}
+                    </Text>
+                </View>
+
+                <View style={{ flexDirection: "row", gap: 16 }}>
+                    <TouchableOpacity onPress={() => {}}>
+                        <Ionicons name="filter-circle-outline" size={26} color={colors.fontLight} />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => {}}>
+                        <Ionicons name="notifications-outline" size={26} color={colors.fontLight} />
+                    </TouchableOpacity>
+                    {/* TODO: fix link href*/}
+                    <Link href={`/`} asChild>
+                        <TouchableOpacity>
+                            <MaterialCommunityIcons
+                                name="dots-horizontal"
+                                size={26}
+                                color={colors.fontLight}
+                            />
+                        </TouchableOpacity>
+                    </Link>
+                </View>
+            </View>
+        </BlurView>
+    );
+
+    return (
+        <ScrollView
+            style={{
+                backgroundColor: bg,
+                paddingTop: headerHeight,
+                flex: 1,
+            }}
+        >
+            <Stack.Screen
+                options={{
+                    title: board?.title,
+                    headerTransparent: true,
+                    header: () => <CustomHeader />,
+                }}
+            />
+            <Text>asdasdasd</Text>
+            {/*{board && <BoardArea board={board} />}*/}
+        </ScrollView>
+    );
+}
